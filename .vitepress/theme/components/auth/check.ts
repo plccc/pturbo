0850590b8c5f459984579705921747864a8744dc
psq <psq@qiruankeji.cn>
2024-08-02 10:02:37 +0800

    fix: deploy.yml & 调整dist

--- .vitepress/theme/components/auth/check.ts
+++
@@ -1,9 +0,0 @@
-import CryptoJS from 'crypto-js'
-
-export class CryptoHelper {
-  static SECRET_KEY = import.meta.env.VITE_SECRET_KEY as string
-
-  static hmacSHA256(data: string): string {
-    return CryptoJS.HmacSHA256(data, CryptoHelper.SECRET_KEY).toString()
-  }
-}
